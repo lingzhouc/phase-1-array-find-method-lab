69bbd618f621f84f50b6d402268a6736e1e93027
LingZhouChen <lzcciao@gmail.com>
2023-09-24 23:45:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// data for denver broncos football team
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  
+  // return year when win occurred
+  // return undefined if no win found
+
+function superbowlWin(arrayObject) {
+    let win = arrayObject.find(obj => obj.result === "W")
+    // checking if win is true; true if not undefined
+    if (win) {
+      return win.year;
+    }
+    // returning undefined if no win
+    return win;
+}
+
+console.log(superbowlWin(record));
+
